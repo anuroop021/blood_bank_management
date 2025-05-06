2ef40a32e2d7546456ff35d464dcacdc69f3aecd
Koushik Pula <koushik2pula@gmail.com>
2025-05-06 13:21:05 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -45,8 +45,21 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(helmet());
 app.use(morgan('dev'));
 
+// app.use(cors({
+//   origin: 'http://localhost:3000', 
+//   credentials: true
+// }));
+
+const allowedOrigins = ['http://localhost:3000', 'https://blood-bank-management-42gz.vercel.app'];
+
 app.use(cors({
-  origin: 'http://localhost:3000', 
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 
