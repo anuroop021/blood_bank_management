797e708a7241d1d3272034a99d4f522eddec1bc3
Koushik-Pula <154884999+Koushik-Pula@users.noreply.github.com>
2025-05-07 12:40:08 +0530

    Update server

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ app.use(helmet());
 app.use(morgan('dev'));
 
  app.use(cors({
-  origin: 'https://blood-bank-management-42gz-72ytjyd0m-koushik-pulas-projects.vercel.app', 
+  origin: 'https://blood-bank-management-42gz-nc065ska7-koushik-pulas-projects.vercel.app', 
   credentials: true
 }));
 
